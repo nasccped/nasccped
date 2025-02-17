132f86c0454b27323ce27558d602ab7f1a77b643
NASCC <pedro.main.tec@gmail.com>
2025-02-17 19:29:22 -0300

    [TESTED] Testing option selection

--- resumes.py
+++ resumes.py
@@ -4,10 +4,29 @@ from scrlib.visuals import (
     print_menu_options
 )
 
+from scrlib.inputs import get_menu_option_by_strkey
+
 def main():
     terminal_clear()
     print_banner()
+    print(
+        "  \x1b[1;37m" + "Choose an option"
+    )
+    print(
+        "  ----------------"
+        + "\x1b[0m"
+    )
     print_menu_options()
+    print()
+    target_enum = input("  > ")
+    target_enum = get_menu_option_by_strkey(target_enum)
+    print()
+    print(
+        "  You selected "
+        + ("\x1b[1;31mNone" if target_enum is None else ("\x1b[1;34m" + target_enum.name))
+        + "\x1b[0m"
+    )
+    print()
 
 if __name__ == "__main__":
     main()
