f5315cf3ec4a61f5b9f65cc3275c3a9161cea6bc
NASCC <pedro.main.tec@gmail.com>
2025-02-17 19:07:15 -0300

    [UPDATED] Implement MenuOptions display

--- scrlib/visuals.py
+++ scrlib/visuals.py
@@ -1,3 +1,4 @@
+from .menu_options import MenuOptions
 
 def terminal_clear():
     print("\x1b[2J\x1b[H", end="")
@@ -12,3 +13,16 @@ def print_banner():
     )
     print("\x1b[1;37m" + ("=" * 70))
     print("\x1b[0m")
+
+def print_menu_options():
+    
+    men_opt = {
+        str(opt.value[0]) + "." : opt.value[1] for opt in MenuOptions
+    }
+
+    for (k, v) in men_opt.items():
+        print(
+            "  "
+            + "\x1b[1;33m" + k + " "
+            + "\x1b[0m"    + v
+        )
