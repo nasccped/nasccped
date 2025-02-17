90f4d2117dd8510b9697d39ba973a3fe08fc115d
NASCC <pedro.main.tec@gmail.com>
2025-02-17 19:07:32 -0300

    [UPDATED] Calling MenuOptions at main

--- resumes.py
+++ resumes.py
@@ -1,11 +1,13 @@
 from scrlib.visuals import (
     terminal_clear,
-    print_banner
+    print_banner,
+    print_menu_options
 )
 
 def main():
     terminal_clear()
     print_banner()
+    print_menu_options()
 
 if __name__ == "__main__":
     main()
