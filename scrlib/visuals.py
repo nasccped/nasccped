3d8f9bc038a78f4dd019f38bcf0d3ba4c807784b
NASCC <pedro.main.tec@gmail.com>
2025-02-17 18:45:15 -0300

    [UPDATED] New function (print_banner)

--- scrlib/visuals.py
+++ scrlib/visuals.py
@@ -1,3 +1,14 @@
 
 def terminal_clear():
     print("\x1b[2J\x1b[H", end="")
+
+def print_banner():
+    print("\x1b[1;37m" + ("=" * 70))
+    print(
+        " @"
+        + "\x1b[33m" + "nasccped's "
+        + "\x1b[37m" + "| "
+        + "\x1b[32m" + "resumes"
+    )
+    print("\x1b[1;37m" + ("=" * 70))
+    print("\x1b[0m")
