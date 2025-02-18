e2a8607e863fbe98a39c5f6741d84490d80f6759
NASCC <pedro.main.tec@gmail.com>
2025-02-18 19:41:41 -0300

    [FIXED] Ident issues

--- scrlib/menu_option.py
+++ scrlib/menu_option.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class MenuOption(Enum):
-    CHECK  = (1, "check \x1b[1;32m`latest`\x1b[0m "
+    CHECK = (1, "check \x1b[1;32m`latest`\x1b[0m "
                  + "<-> \x1b[1;32m`all`\x1b[0m dirs")
 
     UPDATE = (2, "update \x1b[1;32m`dir`\x1b[0m "
                  + "content with latest ones at "
                  + "\x1b[1;32m`all`\x1b[0m")
 
-    QUIT   = (3, "\x1b[1;31mquit\x1b[0m program")
+    QUIT = (3, "\x1b[1;31mquit\x1b[0m program")
